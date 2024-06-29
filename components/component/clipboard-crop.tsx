c3e5682849cf07ad8b4e9f0c8e58b9d23ce3e8f9
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-06-29 03:47:27 -0300

    feat: 🎸 clipboard crop + otp

---
+++ components/component/clipboard-crop.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+
+import { Check, CopyCheckIcon, CopyIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+interface ClipboardCropProps {
+  otp: string;
+}
+export function ClipboardCrop({ otp }: ClipboardCropProps) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(otp);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="outline"
+            onClick={handleCopy}
+            className={`tracking-[0.5em] ${copied && 'border-green-500 focus:bg-green-100 bg-green-100'}`}
+          >
+            {otp}{' '}
+            {copied ? (
+              <CopyCheckIcon className="ml-2 h-4 w-4" />
+            ) : (
+              <CopyIcon className="ml-2 h-4 w-4" />
+            )}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Click to copy</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
+
+function ClipboardIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
+      <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+    </svg>
+  );
+}
