a867e91f5285b81eb6e17a0c0c9f35bbb2ee1623
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-06-30 19:27:05 -0300

    feat: 🎸 added arch report page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
 import { Globe } from 'lucide-react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
+import Footer from '@/components/organisms/footer';
 import OrganizationList from '@/components/organisms/organization-list';
 import { fetchOrganizations } from '@/server/actions/organization';
 
@@ -27,37 +28,7 @@ export default async function Home() {
           <OrganizationList />
         </section>
       </HydrationBoundary>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-5 md:px-56 border-t fixed bottom-0 left-0 bg-background ">
-        <p className="text-xs text-muted-foreground">
-          &copy; 2024 Task circle. All rights reserved.
-        </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link
-            href="https://github.com/caiotheodoro"
-            className="text-xs hover:underline underline-offset-4"
-            prefetch={false}
-            target="_blank"
-          >
-            <FaGithub className="w-5 h-5" />
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/caiotheodoro1/"
-            className="text-xs hover:underline underline-offset-4"
-            prefetch={false}
-            target="_blank"
-          >
-            <FaLinkedin className="w-5 h-5" />
-          </Link>
-          <Link
-            href="https://caiotheodoro.dev"
-            className="text-xs hover:underline underline-offset-4"
-            prefetch={false}
-            target="_blank"
-          >
-            <Globe className="w-5 h-5" />
-          </Link>
-        </nav>
-      </footer>
+      <Footer />
     </main>
   );
 }
