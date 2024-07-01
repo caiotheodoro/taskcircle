6b88a082e8e3f6780b902a61bffac3240e2c55df
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-07-01 01:12:27 -0300

    feat: 🎸 added real time to posts and org invites

--- components/organisms/architecture-report.tsx
+++ components/organisms/architecture-report.tsx
@@ -4,7 +4,6 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { Badge } from '../ui/badge';
 import Footer from './footer';
@@ -73,7 +72,7 @@ export default function ArchitectureReport() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12 md:py-12 lg:py-24">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -92,6 +91,34 @@ export default function ArchitectureReport() {
             <div className="mx-auto  max-w-3xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
               <Image
                 src="/sd.svg"
+                width={400}
+                height={350}
+                alt="Sequence Diagram"
+                className="mx-auto rounded-xl object-cover object-center sm:w-full"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-12  md:py-12 lg:py-24">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
+                  Sequence Diagram
+                </div>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Real-time broadcasting
+                </h2>
+                <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  The broadcasting is responsible for showing new posts in real
+                  time, also, it broadcasts new invites to the organization
+                  administator
+                </p>
+              </div>
+            </div>
+            <div className="mx-auto  max-w-3xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+              <Image
+                src="/broadcast.svg"
                 width={400}
                 height={350}
                 alt="Sequence Diagram"
