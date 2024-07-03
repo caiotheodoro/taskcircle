d0436aa181ba0eb8fcae50c2665c543063a64ebf
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-07-03 03:09:52 -0300

    feat: 🎸 settings hooks

---
+++ hooks/settings.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { listSettings } from '@/server/actions/settings';
+
+export function useGetOrgSettings(org_name: string) {
+  return useQuery({
+    queryFn: async () => listSettings({ org_name }),
+    queryKey: ['settings'],
+    staleTime: 1000 * 60 * 5,
+    select: (data) => data.data,
+  });
+}
